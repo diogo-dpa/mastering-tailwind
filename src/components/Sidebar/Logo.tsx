0edb1f38a1403f78202e9fff3931734b3a1a3008
Diogo Almazan <diogodpa@outlook.com>
2023-12-23 15:42:58 -0300

    Tabs added

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -203,7 +203,7 @@ export function Logo() {
           </clipPath>
         </defs>
       </svg>
-      <span className="sr-only">Untitle UI</span>
+      <span>Untitle UI</span>
     </strong>
   )
 }
